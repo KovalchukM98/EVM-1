de6350a61d51db7eda81fe5bfe17e42ac9b573a8
Фадин Владислав Максимович <sad_456@ngs.ru>
2021-02-17 23:27:05 +0700

    main changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,45 @@
 #include "header/sc.h"
 #include "header/myTerm.h"
+#include "header/myBigChars.h"
+//#include <ncurses.h>
 
 int main()
 {
-    int *arr = sc_memoryInit();
-    int *command = new int;
-    int *sc_command = new int;
-    int *operand = new int;
-    sc_memorySet(101, 1);
-    sc_memorySet(10, 1);
-    int mem_value = 0;
-    sc_memoryGet(10, &mem_value);
-    std::cout << mem_value << "\n\n";
-    sc_memoryGet(101, &mem_value);
-    const char *filename = "file.txt";
-    sc_memorySave(filename);
-    sc_memoryLoad(filename);
-    sc_memoryGet(10, arr);
-    sc_commandEncode(51, 89, sc_command);
-    std::cout << "\nsc_command(10)= " << *sc_command << "\n";
-    sc_commandDecode(*sc_command, command, operand);
-    sc_regInit();
-    sc_regSet(1,1);
-    int *reg_value = new int;
-    sc_regGet(1,reg_value);
+    // int *arr = sc_memoryInit();
+    // int *command = new int;
+    // int *sc_command = new int;
+    // int *operand = new int;
+    // sc_memorySet(101, 1);
+    // sc_memorySet(10, 1);
+    // int mem_value = 0;
+    // sc_memoryGet(10, &mem_value);
+    // std::cout << mem_value << "\n\n";
+    // sc_memoryGet(101, &mem_value);
+    // const char *filename = "file.txt";
+    // sc_memorySave(filename);
+    // sc_memoryLoad(filename);
+    // sc_memoryGet(10, arr);
+    // sc_commandEncode(51, 89, sc_command);
+    // std::cout << "\nsc_command(10)= " << *sc_command << "\n";
+    // sc_commandDecode(*sc_command, command, operand);
+    // sc_regInit();
+    // sc_regSet(1,1);
+    // int *reg_value = new int;
+    // sc_regGet(1,reg_value);
+    // mt_clrscr();
+    // escape_commands();
+    // mt_setfgcolor(GREEN);
+    // mt_setbgcolor(BLUE);
+    // std::cout << "Check string.\E[00m\n";
+    // int col = 0, row = 0;
+    // mt_getscreensize(row, col);
+    // mt_gotoXY(10, 10);
     mt_clrscr();
-    escape_commands();
-    mt_setfgcolor(GREEN);
-    mt_setbgcolor(BLUE);
-    std::cout << "Check string.\E[00m\n";
-    int col = 0, row = 0;
-    mt_getscreensize(row, col);
-    mt_clrscr();
-    mt_gotoXY(2, 2);
-    
+    bc_box(1, 5, 8, 8);
+    char* ch = new char[3];
+    strcpy(ch, "a");
+    bc_printA(ch);
+    printf("\n");
+    temp();
     return 0;
 }
